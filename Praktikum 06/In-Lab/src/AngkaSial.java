d5ee2643f4450bd18f9b850006ea27d3ab887bb7
Opank <opaw1426@gmail.com>
2026-04-13 14:03:05 +0700

    update jawaban soal praktikum pada Asersi2 dan AngkaSial

--- Praktikum 06/In-Lab/src/AngkaSial.java
+++ Praktikum 06/In-Lab/src/AngkaSial.java
@@ -21,3 +21,22 @@ public class AngkaSial {
         }
     }
 }
+
+/*
+===================================================================
+JAWABAN PERTANYAAN PRAKTIKUM:
+
+1. Ketika eksepsi terjadi, apakah baris 12 (System.out.println(angka+" bukan angka sial");) dieksekusi?
+Jawaban: 
+TIDAK. Ketika angka 13 dimasukkan, program mengeksekusi baris 10 yaitu melempar 
+eksepsi (throw). Saat hal ini terjadi, aliran program langsung terhenti dan keluar 
+dari method cobaAngka untuk mencari blok catch, sehingga baris 12 otomatis dilewati.
+
+2. Apakah baris 21 (as.cobaAngka(12);) dieksekusi?
+Jawaban: 
+TIDAK. Pada baris 20 (as.cobaAngka(13);), eksepsi telah dilemparkan. Dalam mekanisme 
+try-catch, begitu sebuah eksepsi terjadi di dalam blok try, sisa kode di bawahnya 
+(dalam blok try yang sama) akan dihentikan. Program langsung melompat ke blok catch, 
+sehingga baris 21 tidak akan pernah sempat dieksekusi.
+===================================================================
+*/
--- Praktikum 06/In-Lab/src/Asersi2.java
+++ Praktikum 06/In-Lab/src/Asersi2.java
@@ -23,3 +23,18 @@ public class Asersi2 {
         System.out.println("keliling lingkaran = " + kelilingLingkaran);
     }
 }
+
+/*
+===================================================================
+JAWABAN PERTANYAAN PRAKTIKUM:
+Secara konsep, ada yang kurang tepat pada program Asersi2 di atas.
+
+Konsep penggunaan Asersi pada program Asersi2 kurang tepat karena asersi 
+tidak seharusnya digunakan untuk memvalidasi input/argumen. Asersi secara default 
+dimatikan oleh Java saat program berjalan normal. Jika program dijalankan 
+tanpa argumen '-enableassertions', maka baris validasi akan dilewati sepenuhnya. 
+Akibatnya, objek Lingkaran akan tetap dibuat dengan nilai jari-jari = 0, dan 
+kelilingnya menjadi 0 tanpa ada peringatan error. Untuk validasi nilai seperti ini, 
+praktik yang benar adalah menggunakan Eksepsi (misalnya IllegalArgumentException).
+===================================================================
+*/
